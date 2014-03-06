3798cfa45ec3634a15a476af2f691768b1e309e1
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-03-06 16:37:03 +0000

    initial sanity test using config

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
 scalaVersion := "2.10.0"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+
+libraryDependencies += "com.typesafe" % "config" % "1.2.0"
--- src/test/scala/test/ContentApiSanityTest.scala
+++ src/test/scala/test/ContentApiSanityTest.scala
@@ -2,6 +2,7 @@ package test
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import com.typesafe.config.ConfigFactory
 
 
 class ContentApiSanityTest extends FlatSpec with ShouldMatchers with Http {
@@ -9,12 +10,16 @@ class ContentApiSanityTest extends FlatSpec with ShouldMatchers with Http {
   val CacheControl = """public, max-age=(\d+)""".r
 
   "Content api" should "serve gzipped" in {
+    val conf = ConfigFactory.load()
+
 
     val connection = GET(
-      "http://content.guardianapis.com/search?format=json",
+      conf.getString("content-api-sanity-tests.host")+"/search?format=json",
       compress = true
     )
 
     connection.bodyFromGzip should include("results")
   }
 }
+
+
