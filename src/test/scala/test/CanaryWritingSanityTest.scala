8a5ceadca19c94b9c5bc4d7339c4805365f4a6d9
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-04-10 15:28:58 +0100

    indentation

--- src/test/scala/test/CanaryWritingSanityTest.scala
+++ src/test/scala/test/CanaryWritingSanityTest.scala
@@ -13,8 +13,8 @@ import org.scalatest.concurrent.Eventually._
 
 class CanaryWritingSanityTest extends FlatSpec with Matchers with ScalaFutures {
   implicit val defaultPatience = PatienceConfig(timeout = Span(2, Seconds), interval = Span(1, Second))
-   val now = new DateTime();
-   val capiDateStamp = now.toString(ISODateTimeFormat.dateTimeNoMillis().withZoneUTC())
+  val now = new DateTime();
+  val capiDateStamp = now.toString(ISODateTimeFormat.dateTimeNoMillis().withZoneUTC())
 
   val collectionJSON=s"""  |{
                        |               "type": "features",
@@ -59,27 +59,22 @@ class CanaryWritingSanityTest extends FlatSpec with Matchers with ScalaFutures {
   val sanityConfig = conf.getConfig("content-api-sanity-tests")
   conf.checkValid(ConfigFactory.defaultReference(), "content-api-sanity-tests")
 
-  "PUTting a Collection" should "return a 202" in{
-  val httpRequest = request(sanityConfig.getString("write-host")+"collections/canary")
-    .withAuth(sanityConfig.getString("write-username"),sanityConfig.getString("write-password"),AuthScheme.BASIC)
-    .withHeaders("Content-Type" -> "application/json")
-    .put(collectionJSON)
+  "PUTting a Collection" should "return a 202" in {
+    val httpRequest = request(sanityConfig.getString("write-host")+"collections/canary")
+      .withAuth(sanityConfig.getString("write-username"),sanityConfig.getString("write-password"),AuthScheme.BASIC)
+      .withHeaders("Content-Type" -> "application/json")
+      .put(collectionJSON)
 
-  whenReady(httpRequest) { result =>
-    result.status should equal(202)
+    whenReady(httpRequest) { result =>
+      result.status should equal(202)
     }
   }
 
   "GETting the collection" should "show the updated timestamp" in {
-        eventually(timeout(Span(60, Seconds)), interval(Span(1, Second))) {
-          val httpRequest = request(sanityConfig.getString("host")+"collections/canary").get()
-          whenReady(httpRequest) { result =>
-          result.body should include (capiDateStamp) }
+    eventually(timeout(Span(60, Seconds)), interval(Span(1, Second))) {
+      val httpRequest = request(sanityConfig.getString("host")+"collections/canary").get()
+      whenReady(httpRequest) { result =>
+      result.body should include (capiDateStamp) }
     }
   }
 }
-
-
-
-
-
