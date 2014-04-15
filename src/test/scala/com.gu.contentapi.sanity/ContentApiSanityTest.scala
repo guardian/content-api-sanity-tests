99db8b04da392b385ed0eb269b5ac31e451bcde9
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-04-15 15:18:56 +0100

    reduce whitespace

--- src/test/scala/com.gu.contentapi.sanity/ContentApiSanityTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/ContentApiSanityTest.scala
@@ -7,14 +7,10 @@ class ContentApiSanityTest extends FlatSpec with Matchers with HttpHandler {
   val CacheControl = """public, max-age=(\d+)""".r
 
   "Content api" should "serve gzipped" in {
-
-
-
     val connection = GET(
       Config.host + "search?format=json&api-key=" + Config.apiKey,
       compress = true
     )
-
     connection.bodyFromGzip should include("results")
   }
 }
