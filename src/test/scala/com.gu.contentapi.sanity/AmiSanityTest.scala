d68b73ea3e9655f09d0bcc59a26edeecffa5b60e
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-04-15 15:22:14 +0100

    added comment to better describe test

--- src/test/scala/com.gu.contentapi.sanity/AmiSanityTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/AmiSanityTest.scala
@@ -8,7 +8,7 @@ class AmiSanityTest extends FlatSpec with Matchers with HttpHandler {
 
 
   "www.theguardian.com" should "be on the latest version of the AMI" in {
-
+    // this test looks on the page for the unique ID of the EU Ireland PV EBS-Backed 64-bit image
     // if this test fails you probably need to update the stack to the latest AMI (and update this test)
 
     val connection = GET(
