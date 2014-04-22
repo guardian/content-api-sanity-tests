f8017a66c2bef97620043bbe31d9dd24e783bba7
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-04-22 15:22:26 +0100

    refactor to remove Grant's HTTP library, set compression enable in WS conf

--- src/test/scala/com.gu.contentapi.sanity/ContentApiSanityTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/ContentApiSanityTest.scala
@@ -1,17 +1,17 @@
 package com.gu.contentapi.sanity
 
 import org.scalatest.{Matchers, FlatSpec, matchers}
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 
-class ContentApiSanityTest extends FlatSpec with Matchers with HttpHandler {
-
-  val CacheControl = """public, max-age=(\d+)""".r
+class ContentApiSanityTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
 
   "Content api" should "serve gzipped" in {
-    val connection = GET(
-      Config.host + "search?format=json&api-key=" + Config.apiKey,
-      compress = true
-    )
-    connection.bodyFromGzip should include("results")
+    // GZip Compression is enabled in application.conf
+
+    val httpRequest = request(Config.host + "search?format=json&api-key=" + Config.apiKey).get()
+    whenReady(httpRequest) { result =>
+      result.body should include ("results")
+    }
   }
 }
 
