ff8d4efc9e6daa182a9095e91a22377b4e464b4f
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-04-22 16:23:01 +0100

    refactor to parse json

--- src/test/scala/com.gu.contentapi.sanity/PreviewContentSetNotInLiveTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/PreviewContentSetNotInLiveTest.scala
@@ -3,6 +3,7 @@ package com.gu.contentapi.sanity
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import play.api.libs.json.Json
 
 
 class PreviewContentSetNotInLiveTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
@@ -13,4 +14,18 @@ class PreviewContentSetNotInLiveTest extends FlatSpec with Matchers with ScalaFu
       result.body should include (""""results":[]""")
     }
   }
+
+  "GETting the preview content set JSON" should "show no results on live" in {
+    val httpRequest = request(Config.host + "search?content-set=preview").get()
+    whenReady(httpRequest) { result =>
+      val json = Json.parse(result.body)
+      val total = (json \ "response" \ "total").as[Long]
+      val results = Json.stringify(json \ "response" \ "results")
+      total should equal (0)
+      results.toString() should equal ("[]")
+    }
+  }
+
+
+
 }
