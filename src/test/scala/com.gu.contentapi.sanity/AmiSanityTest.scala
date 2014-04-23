22498a16502582bf75b6e76560066c94dbf82246
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-04-23 11:28:20 +0100

    tweak teamcity reporting

--- src/test/scala/com.gu.contentapi.sanity/AmiSanityTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/AmiSanityTest.scala
@@ -9,25 +9,25 @@ import org.scalatest.exceptions.TestFailedException
 class AmiSanityTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
 
   "www.theguardian.com" should "be on the latest version of the AMI" in {
-    println("##teamcity[message text='AMI Test Failed' errorDetails='Check amazon for the latest AMI.' status='WARNING']")
+    println("##teamcity[message text='AMI Test Failed' errorDetails='Check amazon for the latest AMI.' status='ERROR']")
     // this test looks on the page for the unique ID of the EU Ireland PV EBS-Backed 64-bit image
     // if this test fails you probably need to update the stack to the latest AMI (and update this test)
 
     val httpRequest = request("http://aws.amazon.com/amazon-linux-ami/").get
 
-    teamCityNotifier("Check amazon for the latest AMI") {
+    teamCityNotifier("Check amazon for the latest AMI","AMI in test did not match latest AMI on Amazon website") {
       whenReady(httpRequest) { result =>
         result.body should include ("ami-FAIL")
       }
     }
   }
 
-  def teamCityNotifier(s: String)(test: =>Unit){
+  def teamCityNotifier(testName: String,testErrorMessage: String)(test: =>Unit){
     try{
       test
     } catch {
       case e: TestFailedException =>  {
-        println(s"##teamcity[message text='$e.getMessage' errorDetails='$s' status='ERROR']")
+        println(s"##teamcity[testFailed name='$testName' message='$testErrorMessage']")
         throw e
       }
     }
