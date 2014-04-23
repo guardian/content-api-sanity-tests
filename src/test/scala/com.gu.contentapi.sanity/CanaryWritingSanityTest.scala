1a66dec136c3d5d9220cb36789165a37506f2094
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-04-23 12:37:20 +0100

    make canary retryable

--- src/test/scala/com.gu.contentapi.sanity/CanaryWritingSanityTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/CanaryWritingSanityTest.scala
@@ -1,7 +1,6 @@
 package com.gu.contentapi.sanity
 
-import org.scalatest.FlatSpec
-import org.scalatest.Matchers
+import org.scalatest.{Retries, FlatSpec, Matchers}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.time.{Seconds, Second, Span}
 import com.ning.http.client.Realm.AuthScheme
@@ -10,15 +9,24 @@ import org.joda.time.format.{ISODateTimeFormat}
 import org.scalatest.concurrent.Eventually._
 import scala.io.Source
 import scala.io.Source
+import org.scalatest.tagobjects.Retryable
 
 
-class CanaryWritingSanityTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
+class CanaryWritingSanityTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience with Retries {
+
+  override def withFixture(test: NoArgTest) = {
+    if (isRetryable(test))
+      withRetry { super.withFixture(test) }
+    else
+      super.withFixture(test)
+  }
+
   val now = new DateTime()
   val collectionJSON = Source.fromURL(getClass.getResource("/CanaryCollection.json")).getLines.mkString
   val capiDateStamp = now.toString(ISODateTimeFormat.dateTimeNoMillis().withZoneUTC())
   val collectionJSONWithNowTimestamp = collectionJSON.replace("2013-10-15T11:42:17Z",capiDateStamp)
 
-  "PUTting a Collection" should "return a 202" in {
+  "PUTting a Collection" should "return a 202" taggedAs(Retryable) in {
     val httpRequest = request(Config.writeHost + "collections/canary")
       .withAuth(Config.writeUsername,Config.writePassword,AuthScheme.BASIC)
       .withHeaders("Content-Type" -> "application/json")
@@ -30,7 +38,7 @@ class CanaryWritingSanityTest extends FlatSpec with Matchers with ScalaFutures w
     }
   }
 
-  "GETting the collection" should "show the updated timestamp" in {
+  "GETting the collection" should "show the updated timestamp" taggedAs(Retryable) in {
       val httpRequest = request(Config.host + "collections/canary").get
       whenReady(httpRequest) { result =>
           result.body should include (capiDateStamp)
