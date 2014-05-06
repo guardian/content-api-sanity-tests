55d82774f4961461f27f59a517d46023e57d531e
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-06 15:06:11 +0100

    add jre test

---
+++ src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
@@ -0,0 +1,17 @@
+package com.gu.contentapi.sanity
+
+import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+
+class JREVersionTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
+  {
+    "The Content API" should "be using the latest JRE" in {
+      val httpRequest = request("http://java.com/applet/JreCurrentVersion2.txt").get
+      teamCityNotifier("Check for the latest JRE", "JRE in test did not match latest AMI listed by java.com") {
+        whenReady(httpRequest) { result =>
+          result.body should include ("1.7.0_55")
+        }
+      }
+    }
+  }
+}
