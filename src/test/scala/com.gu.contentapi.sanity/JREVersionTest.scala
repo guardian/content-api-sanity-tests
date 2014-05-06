8d06e39486aac5a76f4f9b4df309fa39cdcc368e
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-06 15:07:00 +0100

    fix typo

--- src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
@@ -7,7 +7,7 @@ class JREVersionTest extends FlatSpec with Matchers with ScalaFutures with Integ
   {
     "The Content API" should "be using the latest JRE" in {
       val httpRequest = request("http://java.com/applet/JreCurrentVersion2.txt").get
-      teamCityNotifier("Check for the latest JRE", "JRE in test did not match latest AMI listed by java.com") {
+      teamCityNotifier("Check for the latest JRE", "JRE in test did not match latest JRE listed by java.com") {
         whenReady(httpRequest) { result =>
           result.body should include ("1.7.0_55")
         }
