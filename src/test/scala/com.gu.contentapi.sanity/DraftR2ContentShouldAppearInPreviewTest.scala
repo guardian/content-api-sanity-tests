6cf56bf75f833a319a7a159d96fa5e4360370e89
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-08 11:20:13 +0100

    posting a draft article

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -4,17 +4,27 @@ import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.selenium.{HtmlUnit, WebBrowser}
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.WebDriver
+import org.scalatest.concurrent.{IntegrationPatience, Eventually}
 
-class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with WebBrowser {
+class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with Eventually with IntegrationPatience with WebBrowser {
 
   implicit val webDriver: WebDriver = new HtmlUnitDriver
 
-  "With crendentials I" should "be able to login to R2" in {
-    go to (Config.r2AdminHost)
+  "With crendentials I" should "be able to create a draft article in R2" in {
+    go to (Config.r2AdminHost + "/tools/article/new")
     pageTitle should be ("Login")
     textField("j_username").value = Config.r2AdminUsername
     pwdField("j_password").value = Config.r2AdminPassword
     submit
-    pageTitle should be ("Ed index home")
+    pageTitle should be ("Article: new")
+    textField("tagPickerInput").value = "Football"
+    click on "tag-picker-add"
+    textField("content.headline").value = "Content API Sanity Test Draft "+java.util.UUID.randomUUID.toString
+    textField("content.page.linkText").value = "Content API Sanity Test Draft "+java.util.UUID.randomUUID.toString
+    textArea("bodyText").value = "Content API Sanity Test Draft "+java.util.UUID.randomUUID.toString
+    click on ("save-content-bottom")
+    eventually{
+      pageTitle should include ("Sanity Test")
+    }
   }
 }
