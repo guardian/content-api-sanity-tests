767f09cd348cc3eb110fc4946fae1c4d5e74f80f
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-08 10:54:41 +0100

    remove unneeded eventually trait

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -4,9 +4,8 @@ import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.selenium.{HtmlUnit, WebBrowser}
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.WebDriver
-import org.scalatest.concurrent.Eventually
 
-class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with Eventually with WebBrowser {
+class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with WebBrowser {
 
   implicit val webDriver: WebDriver = new HtmlUnitDriver
 
@@ -16,7 +15,6 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
     textField("j_username").value = Config.r2AdminUsername
     pwdField("j_password").value = Config.r2AdminPassword
     submit
-    eventually { pageTitle should be
-      "Ed index home" }
+    pageTitle should be ("Ed index home")
   }
 }
