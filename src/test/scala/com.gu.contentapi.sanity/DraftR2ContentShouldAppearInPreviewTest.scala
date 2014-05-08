25b7f9c4ce1974d46d51ebbccc1447c6e38901c2
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-08 12:54:17 +0100

    spacing

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -1,7 +1,7 @@
 package com.gu.contentapi.sanity
 
 import org.scalatest.{Matchers, FlatSpec}
-import org.scalatest.selenium.{HtmlUnit, WebBrowser}
+import org.scalatest.selenium.{WebBrowser}
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.WebDriver
 import org.scalatest.concurrent.{IntegrationPatience, Eventually}
@@ -11,6 +11,7 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
   implicit val webDriver: WebDriver = new HtmlUnitDriver
 
   "With crendentials I" should "be able to create a draft article in R2" in {
+    val headline = "Content API Sanity Test Draft "+ java.util.UUID.randomUUID.toString
     go to (Config.r2AdminHost + "/tools/article/new")
     pageTitle should be ("Login")
     textField("j_username").value = Config.r2AdminUsername
@@ -19,12 +20,12 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
     pageTitle should be ("Article: new")
     textField("tagPickerInput").value = "Football"
     click on "tag-picker-add"
-    textField("content.headline").value = "Content API Sanity Test Draft "+java.util.UUID.randomUUID.toString
-    textField("content.page.linkText").value = "Content API Sanity Test Draft "+java.util.UUID.randomUUID.toString
-    textArea("bodyText").value = "Content API Sanity Test Draft "+java.util.UUID.randomUUID.toString
+    textField("content.headline").value = headline
+    textField("content.page.linkText").value = "Content API Sanity Test Draft " + java.util.UUID.randomUUID.toString
+    textArea("bodyText").value = "Content API Sanity Test Draft " + java.util.UUID.randomUUID.toString
     submit
-    eventually{
-      pageTitle should include ("Sanity Test")
+    eventually {
+      pageTitle should be ("Sanity Test")
     }
   }
 }
