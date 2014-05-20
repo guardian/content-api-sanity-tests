35b046f4a26ce61a5325fbdcca24218083aa3301
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-20 14:22:57 +0100

    add status code check before assertion in case java.com is down

--- src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
@@ -9,7 +9,10 @@ class JREVersionTest extends FlatSpec with Matchers with ScalaFutures with Integ
       val httpRequest = request("http://java.com/applet/JreCurrentVersion2.txt").get
       teamCityNotifier("Check for the latest JRE", "JRE in test did not match latest JRE listed by java.com") {
         whenReady(httpRequest) { result =>
-          result.body should include ("1.7.0_55")
+          if (result.status == 200)
+          {
+            result.body should include ("1.7.0_55")
+          }
         }
       }
     }
