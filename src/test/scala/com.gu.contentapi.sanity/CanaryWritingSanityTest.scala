ad99f3d5ef9be09ffc154e69679b5255a1724ff4
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-21 15:04:08 +0100

    move put response code into val

--- src/test/scala/com.gu.contentapi.sanity/CanaryWritingSanityTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/CanaryWritingSanityTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import com.ning.http.client.Realm.AuthScheme
 import org.joda.time.DateTime
-import org.joda.time.format.{ISODateTimeFormat}
+import org.joda.time.format.ISODateTimeFormat
 import scala.io.Source
 import org.scalatest.exceptions.TestFailedException
 
@@ -17,15 +17,16 @@ class CanaryWritingSanityTest extends FlatSpec with Matchers with ScalaFutures w
   val collectionJSONWithNowTimestamp = collectionJSON.replace("2013-10-15T11:42:17Z", capiDateStamp)
 
   "PUTting and GETting a collection" should "show an updated timestamp" in {
+    val putSuccessResponseCode = 202
     val httpRequest = request(Config.writeHost + "collections/canary")
       .withAuth(Config.writeUsername, Config.writePassword, AuthScheme.BASIC)
       .withHeaders("Content-Type" -> "application/json")
       .put(collectionJSONWithNowTimestamp)
     whenReady(httpRequest) { result =>
-      withClue("Response code was " + result.status + " expected 202") {
-        result.status should equal (202)
+      withClue("Response code was " + result.status + " expected "+ putSuccessResponseCode) {
+        result.status should equal (putSuccessResponseCode)
       }
-      if (result.status == 202) {
+      if (result.status == putSuccessResponseCode) {
         val numOfAttempts = 10
 
         val doesCanaryHaveUpdatedTimestamp = retryNTimes(numOfAttempts, 1000) {
