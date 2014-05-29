9e16ff3199b54bc8e0de0a015189bcc83acbfd20
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-29 10:56:46 +0100

    use ignore annotation

--- src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
@@ -1,21 +1,23 @@
 package com.gu.contentapi.sanity
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest.{Ignore, Matchers, FlatSpec}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.exceptions.TestFailedException
 
+@Ignore
 class JREVersionTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
+
   {
     "The Content API" should "be using the latest JRE" in {
       val httpRequest = request("http://java.com/applet/JreCurrentVersion2.txt").get
-      /*whenReady(httpRequest) { result =>
+      whenReady(httpRequest) { result =>
         if (result.status == 200) {
           result.body should include("1.7.0_55")
         }
         else {
           throw new TestFailedException("Java.com did not return a 200 status code", 1)
         }
-      }*/
+      }
     }
   }
 }
