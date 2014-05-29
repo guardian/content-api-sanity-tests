6a8592c12d4143dfb137f44afddd13ebdf1f92c8
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-05-29 16:40:50 +0100

    start to use multipart post

--- src/test/scala/com.gu.contentapi.sanity/ReadComposerDraftInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/ReadComposerDraftInPreviewTest.scala
@@ -3,16 +3,40 @@ package com.gu.contentapi.sanity
 import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import scala.io.Source
+import java.io.File
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import com.ning.http.multipart.FilePart
+import com.ning.http.multipart.MultipartRequestEntity
+import com.ning.http.multipart.Part
+
 
 class ReadComposerDraftInPreviewTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
 
   "Posting an article into Composer" should "respond with OK" in {
     val httpRequest = request(Config.composerHost + "/incopyintegration/index").withHeaders("User-Agent" -> "curl").get
     whenReady(httpRequest) { result => result.body should equal("incopy Integration...")
-      val postArticle = request(Config.composerHost + "incopyintegration/article/import").withHeaders("User-Agent" -> "curl").post(Source.fromURL(getClass.getResource("/composer_article.xml")).getLines.mkString)
+    }
+
+      val bos = new ByteArrayOutputStream
+    val filename = getClass.getResource("/composer_article.xml").toURI
+    filename should equal ("foo")
+    println(filename)
+      // Build up the Multiparts
+      val parts: Array[Part] = Array(new FilePart("file", new File(filename)))
+
+      // Add it to the MultipartRequestEntity
+      val reqE = new MultipartRequestEntity(parts, null)
+      reqE.writeRequest(bos)
+      val reqIS = new ByteArrayInputStream(bos.toByteArray)
+
+      val data:Array[Byte] = Array()
+      reqIS.read(data)
+
+      val postArticle = request(Config.composerHost + "incopyintegration/article/import").withHeaders("User-Agent" -> "curl").post(data)
       whenReady(postArticle) { result =>
-        result.status should equal(200 )
+        result.body should equal(200 )
       }
     }
-  }
+
 }
