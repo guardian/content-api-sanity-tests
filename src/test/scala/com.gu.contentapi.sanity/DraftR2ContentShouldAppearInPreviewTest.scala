6aa02755ee8ecd5d428896a901a4f6b94788ac84
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-05 15:10:24 +0100

    WIP import using newspaper integration

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -5,27 +5,30 @@ import org.scalatest.selenium.{WebBrowser}
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.WebDriver
 import org.scalatest.concurrent.{IntegrationPatience, Eventually}
+import org.openqa.selenium.firefox.FirefoxDriver
+import scala.io.Source
 
 class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with Eventually with IntegrationPatience with WebBrowser {
 
-  implicit val webDriver: WebDriver = new HtmlUnitDriver
+  implicit val webDriver: WebDriver = new FirefoxDriver
 
   "With crendentials I" should "be able to create a draft article in R2" in {
-    val headline = "Content API Sanity Test Draft "+ java.util.UUID.randomUUID.toString
-    go to (Config.r2AdminHost + "/tools/article/new")
+    val assetPath = getClass.getResource("/TestR2IntegrationArticle.xml").toString
+    go to (Config.r2AdminHost + "/tools/newspaperintegration/index")
     pageTitle should be ("Login")
     textField("j_username").value = Config.r2AdminUsername
     pwdField("j_password").value = Config.r2AdminPassword
     submit
-    pageTitle should be ("Article: new")
-    textField("tagPickerInput").value = "Football"
-    click on "tag-picker-add"
-    textField("content.headline").value = headline
-    textField("content.page.linkText").value = "Content API Sanity Test Draft " + java.util.UUID.randomUUID.toString
-    textArea("bodyText").value = "Content API Sanity Test Draft " + java.util.UUID.randomUUID.toString
-    submit
-    eventually {
-      pageTitle should include ("Sanity Test")
-    }
+    xpath("//form[@action='article/import']/input[@type='file']").webElement.sendKeys(assetPath)
+    click on xpath("//form[@action='article/import']/input[@type='submit']")
+    //check import is successful
+    val result = cssSelector("body").webElement.getText
+    result should include ("OK")
+    val items = result.split(":|;");
+    val importStatus = items(0)
+    val pageId = items(1)
+    importStatus should be ("OK")
+    pageId should be ("435627291")
+    close
   }
 }
