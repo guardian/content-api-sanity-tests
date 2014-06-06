de49eda1c83d847dc25f22a025ad4f3d42c933f8
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-06 17:54:20 +0100

    read from temp file

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -6,19 +6,29 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.WebDriver
 import org.scalatest.concurrent.{ScalaFutures, IntegrationPatience, Eventually}
 import com.ning.http.client.Realm.AuthScheme
+import scala.io.Source
+import scalax.io._
+import org.openqa.selenium.firefox.FirefoxDriver
+import scalax.file.Path
 
 class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with Eventually with IntegrationPatience with WebBrowser with ScalaFutures {
 
-  implicit val webDriver: WebDriver = new HtmlUnitDriver
+  implicit val webDriver: WebDriver = new FirefoxDriver
 
   "Updating a draft in R2" should "show an update in the Preview API" in {
-    val assetPath = getClass.getResource("/TestR2IntegrationArticle.xml").toString
+    val tempFilePath = "/tmp/TestR2IntegrationArticleModified.xml"
+    val path: Path = Path.fromString(tempFilePath)
+    path.deleteIfExists()
+    val r2ArticleXML = Source.fromURL(getClass.getResource("/TestR2IntegrationArticle.xml")).getLines.mkString
+    val modifiedR2ArticleXML = r2ArticleXML.replace("Facebook messaging article", "Content API Sanity Test "/*+java.util.UUID.randomUUID.toString*/)
+    val output:Output = Resource.fromFile(tempFilePath)
+    output.write(r2ArticleXML)
     go to (Config.r2AdminHost + "/tools/newspaperintegration/index")
     pageTitle should be ("Login")
     textField("j_username").value = Config.r2AdminUsername
     pwdField("j_password").value = Config.r2AdminPassword
     submit
-    xpath("//form[@action='article/import']/input[@type='file']").webElement.sendKeys(assetPath)
+    xpath("//form[@action='article/import']/input[@type='file']").webElement.sendKeys("/tmp/TestR2IntegrationArticleModified.xml")
     click on xpath("//form[@action='article/import']/input[@type='submit']")
     //check import is successful
     val result = cssSelector("body").webElement.getText
@@ -29,11 +39,10 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
     importStatus should be ("OK")
     pageId should be ("435627291")
     close
-  }
 
-  val httpRequest = request(Config.previewHostCode+"internal-code/content/435627291").withAuth(Config.previewUsernameCode, Config.previewPasswordCode, AuthScheme.BASIC).get
-  whenReady(httpRequest) { result =>
-    result.body should include ("Facebook messaging article")
+    val httpRequest = request(Config.previewHostCode+"internal-code/content/435627291").withAuth(Config.previewUsernameCode, Config.previewPasswordCode, AuthScheme.BASIC).get
+    whenReady(httpRequest) { result =>
+      result.body should include ("Facebook messaging article")
+    }
   }
-
 }
