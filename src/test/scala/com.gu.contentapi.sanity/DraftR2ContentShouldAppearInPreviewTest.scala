b4b773e4c52f8850f0e058e05896260bd1037b5a
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-06 18:21:35 +0100

    use modified file for test

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -8,23 +8,29 @@ import org.scalatest.concurrent.{ScalaFutures, IntegrationPatience, Eventually}
 import com.ning.http.client.Realm.AuthScheme
 import scala.io.Source
 import scalax.io._
-import org.openqa.selenium.firefox.FirefoxDriver
 import scalax.file.Path
+import org.scalatest.time.{Seconds, Span}
 
 class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with Eventually with IntegrationPatience with WebBrowser with ScalaFutures {
 
-  implicit val webDriver: WebDriver = new FirefoxDriver
+  val modifiedHeadline = "Content API Sanity Test " + java.util.UUID.randomUUID.toString
+
+  def doesR2HaveModifiedDraftData = {
+    val httpRequest = request(Config.previewHostCode + "internal-code/content/435627291").withAuth(Config.previewUsernameCode, Config.previewPasswordCode, AuthScheme.BASIC).get
+    whenReady(httpRequest) { result => result.body.contains(modifiedHeadline)}
+  }
+  implicit val webDriver: WebDriver = new HtmlUnitDriver
 
   "Updating a draft in R2" should "show an update in the Preview API" in {
     val tempFilePath = "/tmp/TestR2IntegrationArticleModified.xml"
     val path: Path = Path.fromString(tempFilePath)
     path.deleteIfExists()
     val r2ArticleXML = Source.fromURL(getClass.getResource("/TestR2IntegrationArticle.xml")).getLines.mkString
-    val modifiedR2ArticleXML = r2ArticleXML.replace("Facebook messaging article", "Content API Sanity Test "/*+java.util.UUID.randomUUID.toString*/)
-    val output:Output = Resource.fromFile(tempFilePath)
-    output.write(r2ArticleXML)
+    val modifiedR2ArticleXML = r2ArticleXML.replace("Facebook messaging article", modifiedHeadline)
+    val output: Output = Resource.fromFile(tempFilePath)
+    output.write(modifiedR2ArticleXML)
     go to (Config.r2AdminHost + "/tools/newspaperintegration/index")
-    pageTitle should be ("Login")
+    pageTitle should be("Login")
     textField("j_username").value = Config.r2AdminUsername
     pwdField("j_password").value = Config.r2AdminPassword
     submit
@@ -32,17 +38,18 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
     click on xpath("//form[@action='article/import']/input[@type='submit']")
     //check import is successful
     val result = cssSelector("body").webElement.getText
-    result should include ("OK")
+    result should include("OK")
     val items = result.split(":|;");
     val importStatus = items(0)
     val pageId = items(1)
-    importStatus should be ("OK")
-    pageId should be ("435627291")
+    importStatus should be("OK")
+    pageId should be("435627291")
     close
-
-    val httpRequest = request(Config.previewHostCode+"internal-code/content/435627291").withAuth(Config.previewUsernameCode, Config.previewPasswordCode, AuthScheme.BASIC).get
-    whenReady(httpRequest) { result =>
-      result.body should include ("Facebook messaging article")
+    eventually(timeout(Span(60, Seconds))) {
+      withClue(s"R2 Article with Page ID:$pageId did not show updated headline $modifiedHeadline within 60 seconds") {
+        doesR2HaveModifiedDraftData should be(true)
+      }
     }
   }
+
 }
