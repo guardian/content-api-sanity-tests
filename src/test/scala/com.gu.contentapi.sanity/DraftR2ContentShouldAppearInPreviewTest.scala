b6af764185c02fd5f77542f9fb8009d5d8134f19
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-09 15:46:53 +0100

    refactor test to use multiple methods

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -14,6 +14,7 @@ import org.scalatest.time.{Seconds, Span}
 class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers with Eventually with IntegrationPatience with WebBrowser with ScalaFutures {
 
   val modifiedHeadline = "Content API Sanity Test " + java.util.UUID.randomUUID.toString
+  val pageId = "435627291"
 
   def doesCAPIHaveModifiedDraftData(capiURI: String) = {
     val httpRequest = request(Config.previewHostCode + capiURI).withAuth(Config.previewUsernameCode, Config.previewPasswordCode, AuthScheme.BASIC).get
@@ -24,28 +25,39 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
 
   "Updating a draft in R2" should "show an update in the Preview API" taggedAs(FrequentTest) in {
     val tempFilePath = "/tmp/TestR2IntegrationArticleModified-" + java.util.UUID.randomUUID().toString + ".xml"
-    val path: Path = Path.fromString(tempFilePath)
+    def cleanup {
+    val pathToTempFile: Path = Path.fromString(tempFilePath)
+    pathToTempFile.deleteIfExists()
+    }
+    cleanup
+    def createModifiedR2Article{
 
-    path.deleteIfExists()
-    val r2ArticleXML = Source.fromURL(getClass.getResource("/TestR2IntegrationArticle.xml")).mkString
-    val modifiedR2ArticleXML = r2ArticleXML.replace("Facebook messaging article", modifiedHeadline)
-    val output: Output = Resource.fromFile(tempFilePath)
-    output.write(modifiedR2ArticleXML)
-    go to (Config.r2AdminHost + "/tools/newspaperintegration/index")
-    pageTitle should be("Login")
-    textField("j_username").value = Config.r2AdminUsername
-    pwdField("j_password").value = Config.r2AdminPassword
-    submit
-    xpath("//form[@action='article/import']/input[@type='file']").webElement.sendKeys(tempFilePath)
-    click on xpath("//form[@action='article/import']/input[@type='submit']")
-    //check import is successful
-    val result = cssSelector("body").webElement.getText
-    val items = result.split(":|;");
-    val importStatus = items(0)
-    val pageId = items(1)
-    importStatus should be("OK")
-    pageId should be("435627291")
-    close
+      val r2ArticleXML = Source.fromURL(getClass.getResource("/TestR2IntegrationArticle.xml")).mkString
+      val modifiedR2ArticleXML = r2ArticleXML.replace("Facebook messaging article", modifiedHeadline)
+      val output: Output = Resource.fromFile(tempFilePath)
+      output.write(modifiedR2ArticleXML)
+    }
+    createModifiedR2Article
+
+    def postModifiedR2ArticleToNewspaperIntegrationEndpoint{
+      go to (Config.r2AdminHost + "/tools/newspaperintegration/index")
+      pageTitle should be("Login")
+      textField("j_username").value = Config.r2AdminUsername
+      pwdField("j_password").value = Config.r2AdminPassword
+      submit
+      xpath("//form[@action='article/import']/input[@type='file']").webElement.sendKeys(tempFilePath)
+      click on xpath("//form[@action='article/import']/input[@type='submit']")
+      //check import is successful
+      val result = cssSelector("body").webElement.getText
+      val items = result.split(":|;");
+      val importStatus = items(0)
+      val importedPageId = (items(1))
+      importStatus should be("OK")
+      importedPageId should be ("435627291")
+      close
+    }
+    postModifiedR2ArticleToNewspaperIntegrationEndpoint
+
     eventually(timeout(Span(60, Seconds))) {
       withClue(s"R2 Article with Page ID:$pageId did not show updated headline $modifiedHeadline within 60 seconds on item endpoint") {
         doesCAPIHaveModifiedDraftData("internal-code/content/"+pageId) should be(true)
@@ -54,6 +66,7 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
         withClue(s"R2 Article with Page ID:$pageId did not show updated headline $modifiedHeadline within 60 seconds on search endpoint") {
           doesCAPIHaveModifiedDraftData("search?use-date=last-modified") should be(true)
         }
+        cleanup
       }
     }
   }
