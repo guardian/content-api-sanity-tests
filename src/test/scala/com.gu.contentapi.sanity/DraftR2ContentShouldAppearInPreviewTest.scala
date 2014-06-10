b4f90cd5ce249e0612d9338194ed0204f3225f62
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-10 10:31:03 +0100

    move functions to bottom with @philwills

--- src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -17,53 +17,14 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
   val pageId = "435627291"
   val tempFilePathString = "/tmp/TestR2IntegrationArticleModified-" + java.util.UUID.randomUUID().toString + ".xml"
   val tempFilePath: Path = Path.fromString(tempFilePathString)
-  
-  def doesCAPIHaveModifiedDraftData(capiURI: String) = {
-    val httpRequest = request(Config.previewHostCode + capiURI).withAuth(Config.previewUsernameCode, Config.previewPasswordCode, AuthScheme.BASIC).get
-    whenReady(httpRequest) { result => result.body.contains(modifiedHeadline)}
-  }
-
-  def login {
-    textField("j_username").value = Config.r2AdminUsername
-    pwdField("j_password").value = Config.r2AdminPassword
-    submit
-  }
 
   implicit val webDriver: WebDriver = new HtmlUnitDriver
 
   "Updating a draft in R2" should "show an update in the Preview API" taggedAs(FrequentTest) in {
-    
-    def cleanup {
-    tempFilePath.deleteIfExists()
-    }
-    def createModifiedR2Article(originalR2XML: String, modifiedR2XMLPath: String) {
-      val r2ArticleXML = originalR2XML
-      val modifiedR2ArticleXML = r2ArticleXML.replace("Facebook messaging article", modifiedHeadline)
-      val output: Output = Resource.fromFile(tempFilePathString)
-      output.write(modifiedR2ArticleXML)
-    }
+
     createModifiedR2Article(Source.fromURL(getClass.getResource("/TestR2IntegrationArticle.xml")).mkString, tempFilePathString)
-
-
-    login
-
-    def postModifiedR2ArticleToNewspaperIntegrationEndpoint(r2XMLPath: String)
-    {
-      go to (Config.r2AdminHost + "/tools/newspaperintegration/index")
-      pageTitle should be("Login")
-
-      xpath("//form[@action='article/import']/input[@type='file']").webElement.sendKeys(r2XMLPath)
-      click on xpath("//form[@action='article/import']/input[@type='submit']")
-      //check import is successful
-      val result = cssSelector("body").webElement.getText
-      val items = result.split(":|;");
-      val importStatus = items(0)
-      val importedPageId = (items(1))
-      importStatus should be("OK")
-      importedPageId should be (pageId)
-      close
-    }
-    postModifiedR2ArticleToNewspaperIntegrationEndpoint(tempFilePathString)
+    login(Config.r2AdminHost + "/tools/newspaperintegration/index")
+    postR2ArticleToNewspaperIntegrationEndpoint(tempFilePathString)
     cleanup
 
     eventually(timeout(Span(60, Seconds))) {
@@ -75,6 +36,45 @@ class DraftR2ContentShouldAppearInPreviewTest extends FlatSpec with Matchers wit
           doesCAPIHaveModifiedDraftData("search?use-date=last-modified") should be(true)
         }
       }
+    }
+
+    def createModifiedR2Article(originalR2XML: String, modifiedR2XMLPath: String) {
+      val r2ArticleXML = originalR2XML
+      val modifiedR2ArticleXML = r2ArticleXML.replace("Facebook messaging article", modifiedHeadline)
+      val output: Output = Resource.fromFile(tempFilePathString)
+      output.write(modifiedR2ArticleXML)
+
+    }
+
+    def login(toolPath: String) {
+      go to (toolPath)
+      pageTitle should be("Login")
+      textField("j_username").value = Config.r2AdminUsername
+      pwdField("j_password").value = Config.r2AdminPassword
+      submit
+    }
+
+    def postR2ArticleToNewspaperIntegrationEndpoint(r2XMLPath: String)
+    {
+      xpath("//form[@action='article/import']/input[@type='file']").webElement.sendKeys(r2XMLPath)
+      click on xpath("//form[@action='article/import']/input[@type='submit']")
+      //check import is successful
+      val result = cssSelector("body").webElement.getText
+      val items = result.split(":|;");
+      val importStatus = items(0)
+      val importedPageId = (items(1))
+      importStatus should be("OK")
+      importedPageId should be (pageId)
+      close
+    }
+
+    def cleanup {
+      tempFilePath.deleteIfExists()
+    }
+
+    def doesCAPIHaveModifiedDraftData(capiURI: String) = {
+      val httpRequest = request(Config.previewHostCode + capiURI).withAuth(Config.previewUsernameCode, Config.previewPasswordCode, AuthScheme.BASIC).get
+      whenReady(httpRequest) { result => result.body.contains(modifiedHeadline)}
     }
   }
 }
