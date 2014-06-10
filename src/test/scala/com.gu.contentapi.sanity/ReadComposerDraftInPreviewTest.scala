62f5c987c4052133d82ea4692b1eadae128a6398
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-10 16:33:30 +0100

    post unique content

--- src/test/scala/com.gu.contentapi.sanity/ReadComposerDraftInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/ReadComposerDraftInPreviewTest.scala
@@ -3,8 +3,15 @@ package com.gu.contentapi.sanity
 import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import scala.sys.process._
+import scalax.io.{Resource, Output}
+import scala.util.Random
+import scala.io.Source
 
 class ReadComposerDraftInPreviewTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
+
+  val modifiedHeadline = "Content API Sanity Test " + java.util.UUID.randomUUID.toString
+  val uniquePageId = new Random().nextInt.toString
+
 
   "GETting the InCopy integration homepage" should "respond with a welcome message" in {
     val httpRequest = request(Config.composerHost + "incopyintegration/index").withHeaders("User-Agent" -> "curl").get
@@ -12,15 +19,22 @@ class ReadComposerDraftInPreviewTest extends FlatSpec with Matchers with ScalaFu
     }
   }
 
-    "POSTting valid Article XML to the Composer integration Endpoint" should "respond with OK" in {
-      val pathToComposerXML  = getClass.getResource("/composer_article.xml").getFile
-      val importEndoint = Config.composerHost + "incopyintegration/article/import"
-      val cmd = Seq("curl", "-sS", "-F", "fileData=@"+pathToComposerXML, importEndoint)
-      val res = cmd.!!
-      val results = res.split(":|;")
-      val status = results(0)
-      val articleID = results(1)
-      status should be ("OK")
-      articleID should be ("534561d7e4b0c12b64c7ea50")
-    }
+  "POSTting valid Article XML to the Composer integration Endpoint" should "respond with OK" in {
+    val fileToImport = createModifiedXML(Source.fromURL(getClass.getResource("/composer_article.xml")).mkString, "7348690|british-gunmakers-arms-sri-lanka|placeholder", uniquePageId)
+    val importEndoint = Config.composerHost + "incopyintegration/article/import"
+    val cmd = Seq("curl", "-sS", "-F", "fileData=@" + fileToImport, importEndoint)
+    val res = cmd.!!
+    val results = res.split(":|;")
+    val status = results(0)
+    val articleID = results(1)
+    status should be("OK")
+  }
+
+  def createModifiedXML(originalXML: String, originalString: String, replacedString: String): String = {
+    val tempFile = java.io.File.createTempFile("TestIntegrationArticleModified-", ".xml")
+    val modifiedArticleXML = originalXML.replaceAll(originalString, replacedString)
+    val output: Output = Resource.fromFile(tempFile)
+    output.write(modifiedArticleXML)
+    tempFile.getAbsolutePath
+  }
 }
