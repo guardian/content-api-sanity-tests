4c3cde74ffd38986e3873653e74cbe21262cdf3c
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-10 14:19:14 +0100

    break down result from Curl

--- src/test/scala/com.gu.contentapi.sanity/ReadComposerDraftInPreviewTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/ReadComposerDraftInPreviewTest.scala
@@ -2,29 +2,25 @@ package com.gu.contentapi.sanity
 
 import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
-import scala.io.Source
-import java.io.File
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
-import com.ning.http.multipart.{StringPart, FilePart, MultipartRequestEntity, Part}
-import com.ning.http.client.{Realm, AsyncHttpClientConfig, AsyncHttpClient}
-import java.nio.file.Paths
-
+import scala.sys.process._
 
 class ReadComposerDraftInPreviewTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
 
-  "Posting an article into Composer" should "respond with OK" in {
-    val httpRequest = request(Config.composerHost + "/incopyintegration/index").withHeaders("User-Agent" -> "curl").get
+  "GETting the InCopy integration homepage" should "respond with a welcome message" in {
+    val httpRequest = request(Config.composerHost + "incopyintegration/index").withHeaders("User-Agent" -> "curl").get
     whenReady(httpRequest) { result => result.body should equal("incopy Integration...")
     }
-
-    val file  = getClass.getResource("/composer_article.xml").getFile
-
-
-
-
-
-
-
   }
+
+    "POSTting valid Article XML to the Composer integration Endpoint" should "respond with OK" in {
+      val pathToComposerXML  = getClass.getResource("/composer_article.xml").getFile
+      val importEndoint = Config.composerHost + "incopyintegration/article/import"
+      val cmd = Seq("curl", "-sS", "-F", "fileData=@"+pathToComposerXML, importEndoint)
+      val res = cmd.!!
+      val results = res.split(":|;")
+      val status = results(0)
+      val articleID = results(1)
+      status should be ("OK")
+      articleID should be ("534561d7e4b0c12b64c7ea50")
+    }
 }
