2070f48b7321845ac7fc32205981a456ed3e2309
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-06-18 14:43:31 +0100

    remove print of modified XML

--- src/test/scala/com.gu.contentapi.sanity/package.scala
+++ src/test/scala/com.gu.contentapi.sanity/package.scala
@@ -47,7 +47,6 @@ package object sanity extends ScalaFutures {
     val tempFile = File.createTempFile("TestIntegrationArticleModified-", ".xml")
     val modifiedArticleXML = originalXML.replaceAll(originalString, replacedString)
     val output: Output = Resource.fromFile(tempFile)
-    println("Modified Article XML is: " + modifiedArticleXML)
     output.write(modifiedArticleXML)
     println("Temp file created at: " + tempFile.getAbsolutePath)
     tempFile.getAbsolutePath
