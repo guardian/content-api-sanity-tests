c57f86cda8a05aec4a6c8f6df26664f1c7285184
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-07-16 12:25:01 +0100

    update java version

--- src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
+++ src/test/scala/com.gu.contentapi.sanity/JREVersionTest.scala
@@ -11,7 +11,7 @@ class JREVersionTest extends FlatSpec with Matchers with ScalaFutures with Integ
       val httpRequest = request("http://java.com/applet/JreCurrentVersion2.txt").get
       whenReady(httpRequest) { result =>
         if (result.status == 200) {
-          result.body should include("1.7.0_60")
+          result.body should include("1.7.0_65")
         }
         else {
           throw new TestFailedException("Java.com did not return a 200 status code", 1)
