2ed86e5e75757768fff1b9a2de83f7b1f124a507
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-07-24 10:51:01 +0100

    create static application

--- src/main/scala/com.gu.contentapi.sanity/Main.scala
+++ src/main/scala/com.gu.contentapi.sanity/Main.scala
@@ -1,7 +1,12 @@
 package com.gu.contentapi
 
+import java.io.File
+
 import com.gu.contentapi.sanity._
 import com.gu.sanity.utils.QuartzScheduler
+import play.api.DefaultApplication
+import play.core.StaticApplication
+import play.core.server.NettyServer
 import scala.concurrent.duration._
 
 object StartScheduledRunner {
@@ -10,6 +15,12 @@ object StartScheduledRunner {
   }
 
   def onStart() {
+    val server = new NettyServer(
+      (new StaticApplication(new File("."))
+      Option(System.getProperty("http.port")).fold(Option(9000))(p => if (p == "disabled") Option.empty[Int] else Option(Integer.parseInt(p))),
+      Option(System.getProperty("https.port")).map(Integer.parseInt(_)),
+      Option(System.getProperty("http.address")).getOrElse("0.0.0.0")
+    )
     QuartzScheduler.start()
     QuartzScheduler schedule("Frequent Tests", runFreqeuntTests) every (30 seconds)
     QuartzScheduler schedule("Infrequent Tests", runInfrequentTests) at "0 0 12 ? * MON-FRI *"
