21f9b53d3bb7c1b8449ef91ca2294b30a528a6cf
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-09-02 11:51:53 +0100

    WIP store and accept incident_key

--- app/com/gu/contentapi/sanity/package.scala
+++ app/com/gu/contentapi/sanity/package.scala
@@ -2,10 +2,11 @@ package com.gu.contentapi
 
 import java.io.File
 import com.ning.http.client.Realm.AuthScheme
+import org.joda.time.{Minutes, Period, DateTime}
 import org.scalatest.Matchers
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.exceptions.TestFailedException
-import play.api.libs.json.Json
+import play.api.libs.json.{JsString, JsValue, Json}
 import play.api.libs.ws.WS
 import play.api.libs.ws.WS.WSRequestHolder
 import scalax.file.Path
@@ -18,21 +19,35 @@ package object sanity extends ScalaFutures with Matchers with IntegrationPatienc
       test
     } catch {
       case tfe: TestFailedException =>
-        pagerDutyAlerter(testName, tfe, tags)
+        val isExistingRecentIncident= new Period(Minutes.minutesBetween(previousIncident.dateTime, new DateTime)).getMinutes < 30
+       val incident = if(isExistingRecentIncident) pagerDutyAlerter(testName, tfe, tags, None) else pagerDutyAlerter(testName, tfe, tags, previousIncident.key)
+
+      lazy val previousIncident = incident
         fail
     }
   }
 
-  def pagerDutyAlerter(testName: String, tfe: TestFailedException, tags: Map[String, Set[String]]) {
+  def pagerDutyAlerter(testName: String, tfe: TestFailedException, tags: Map[String, Set[String]], incident: Option[Incident]): Incident =  {
+    //val isExistingRecentIncident=(new Period(Minutes.minutesBetween(currentIncident._2, new DateTime).getMinutes))
     val isLowPriority = tags.get(testName).map(_.contains("LowPriorityTest")).getOrElse(false)
     val isCODETest = tags.get(testName).map(_.contains("CODETest")).getOrElse(false)
+    val isExistingIncident = incident.isDefined
 
     val serviceKey = if (isLowPriority) Config.pagerDutyServiceKeyLowPriority else Config.pagerDutyServiceKey
 
     val environmentInfo = if (isCODETest) "on environment CODE" else ""
 
+
     val description = (testName + " failed " + environmentInfo + ", the error reported was: " + tfe.getMessage().take(250) + "...")
-  println(description)
+    val dataWithIncidentKey = (
+      "service_key" -> serviceKey,
+      "incident_key" -> incident.get.key,
+      "event_type" -> "trigger",
+      "description" -> description,
+      "client" -> "Content API Sanity Tests",
+      "client_url" -> "https://github.com/guardian/content-api-sanity-tests"
+      )
+
     val data = Json.obj(
       "service_key" -> serviceKey,
       "event_type" -> "trigger",
@@ -40,10 +55,16 @@ package object sanity extends ScalaFutures with Matchers with IntegrationPatienc
       "client" -> "Content API Sanity Tests",
       "client_url" -> "https://github.com/guardian/content-api-sanity-tests"
     )
-    val httpRequest = request("https://events.pagerduty.com/generic/2010-04-15/create_event.json").post(data)
-    whenReady(httpRequest) { result =>
-      result.body should include("success")}
+    val httpRequest = if(isExistingIncident) request("https://events.pagerduty.com/generic/2010-04-15/create_event.json").post(dataWithIncidentKey) else request("https://events.pagerduty.com/generic/2010-04-15/create_event.json").post(data)
 
+    val currentIncident = whenReady(httpRequest) { result =>
+      val pagerDutyResponse: JsValue = Json.parse(result.body)
+      val status = (pagerDutyResponse \ "status").as[String]
+      val incidentKey = (pagerDutyResponse \ "incident_key").as[String]
+      status should be("success")
+      Incident(incidentKey,new DateTime)
+    }
+    currentIncident
   }
   def request(uri: String):WSRequestHolder = WS.url(uri).withRequestTimeout(10000)
 
@@ -78,4 +99,6 @@ package object sanity extends ScalaFutures with Matchers with IntegrationPatienc
       request(Config.host + path + "&api-key=" + Config.apiKey)
     else
       request(Config.host + path + "?api-key=" + Config.apiKey)
+
+  case class Incident(key: String, dateTime: DateTime)
 }
