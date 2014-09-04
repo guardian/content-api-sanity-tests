4d7ccc6fea9d6382b8fc8428bb663e63916c7e4e
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-09-04 16:04:44 +0100

    re-use incident key to avoid opening multiple incidents

--- app/com/gu/contentapi/sanity/package.scala
+++ app/com/gu/contentapi/sanity/package.scala
@@ -2,11 +2,11 @@ package com.gu.contentapi
 
 import java.io.File
 import com.ning.http.client.Realm.AuthScheme
-import org.joda.time.{Minutes, Period, DateTime}
+import org.joda.time.{Seconds, Minutes, DateTime}
 import org.scalatest.Matchers
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.exceptions.TestFailedException
-import play.api.libs.json.{JsString, JsValue, Json}
+import play.api.libs.json.{Json, JsValue}
 import play.api.libs.ws.WS
 import play.api.libs.ws.WS.WSRequestHolder
 import scalax.file.Path
@@ -14,24 +14,47 @@ import scalax.io.{Resource, Output}
 
 package object sanity extends ScalaFutures with Matchers with IntegrationPatience {
 
+ var incidentKeyDateTime: Option[DateTime] = None
+
   def handleException(test: =>Unit)(fail: =>Unit, testName: String, tags: Map[String, Set[String]])= {
     try {
       test
     } catch {
       case tfe: TestFailedException =>
-        val isExistingRecentIncident= new Period(Minutes.minutesBetween(previousIncident.dateTime, new DateTime)).getMinutes < 30
-       val incident = if(isExistingRecentIncident) pagerDutyAlerter(testName, tfe, tags, None) else pagerDutyAlerter(testName, tfe, tags, previousIncident.key)
+        //first run
 
-      lazy val previousIncident = incident
-        fail
+def generateOrReuseIncidentKey:String = {
+  if (!incidentKeyDateTime.isDefined){
+    //generate incident key
+    incidentKeyDateTime = Some(DateTime.now)
+    incidentKeyDateTime.get.toString
+  }
+  else if (Minutes.minutesBetween(incidentKeyDateTime.get, DateTime.now).getMinutes < 30) {
+    //re-use key if is less than 30 minutes since previous incident
+    incidentKeyDateTime.get.toString
+  }
+  else
+  {
+    //after 30 minutes generate new key
+    incidentKeyDateTime = Some(DateTime.now)
+    incidentKeyDateTime.get.toString
+  }
+
+}
+
+
+     pagerDutyAlerter(testName, tfe, tags, generateOrReuseIncidentKey)
+
+     fail
     }
   }
 
-  def pagerDutyAlerter(testName: String, tfe: TestFailedException, tags: Map[String, Set[String]], incident: Option[Incident]): Incident =  {
+  def pagerDutyAlerter(testName: String, tfe: TestFailedException, tags: Map[String, Set[String]], incidentKey: String)=  {
     //val isExistingRecentIncident=(new Period(Minutes.minutesBetween(currentIncident._2, new DateTime).getMinutes))
     val isLowPriority = tags.get(testName).map(_.contains("LowPriorityTest")).getOrElse(false)
     val isCODETest = tags.get(testName).map(_.contains("CODETest")).getOrElse(false)
-    val isExistingIncident = incident.isDefined
+
+   // val recentIncident = incident. (i => new Period(Minutes.minutesBetween(i.dateTime, new DateTime)).getMinutes < 30)
 
     val serviceKey = if (isLowPriority) Config.pagerDutyServiceKeyLowPriority else Config.pagerDutyServiceKey
 
@@ -39,32 +62,31 @@ package object sanity extends ScalaFutures with Matchers with IntegrationPatienc
 
 
     val description = (testName + " failed " + environmentInfo + ", the error reported was: " + tfe.getMessage().take(250) + "...")
-    val dataWithIncidentKey = (
+    //val incidentKey: String= recentIncident.map(i => i.dateTime.toString).getOrElse("")
+    val data = Json.obj (
       "service_key" -> serviceKey,
-      "incident_key" -> incident.get.key,
       "event_type" -> "trigger",
-      "description" -> description,
+      "description" -> "TEST please ignore", //description
       "client" -> "Content API Sanity Tests",
-      "client_url" -> "https://github.com/guardian/content-api-sanity-tests"
+      "client_url" -> "https://github.com/guardian/content-api-sanity-tests",
+      "incident_key" -> incidentKey
       )
 
-    val data = Json.obj(
-      "service_key" -> serviceKey,
-      "event_type" -> "trigger",
-      "description" -> description,
-      "client" -> "Content API Sanity Tests",
-      "client_url" -> "https://github.com/guardian/content-api-sanity-tests"
-    )
-    val httpRequest = if(isExistingIncident) request("https://events.pagerduty.com/generic/2010-04-15/create_event.json").post(dataWithIncidentKey) else request("https://events.pagerduty.com/generic/2010-04-15/create_event.json").post(data)
+    val httpRequest =
+      request("https://events.pagerduty.com/generic/2010-04-15/create_event.json").post(data)
 
-    val currentIncident = whenReady(httpRequest) { result =>
+    whenReady(httpRequest) { result =>
       val pagerDutyResponse: JsValue = Json.parse(result.body)
       val status = (pagerDutyResponse \ "status").as[String]
       val incidentKey = (pagerDutyResponse \ "incident_key").as[String]
       status should be("success")
-      Incident(incidentKey,new DateTime)
+      //incidentKey should be(incident.dateTime.toString)
+      println("incident key is: "+incidentKey)
+      println("status is: "+incidentKey)
+     println("response is: "+ result.body)
+     // Incident(new DateTime)
     }
-    currentIncident
+   // recentIncident.getOrElse(currentIncident)
   }
   def request(uri: String):WSRequestHolder = WS.url(uri).withRequestTimeout(10000)
 
@@ -99,6 +121,3 @@ package object sanity extends ScalaFutures with Matchers with IntegrationPatienc
       request(Config.host + path + "&api-key=" + Config.apiKey)
     else
       request(Config.host + path + "?api-key=" + Config.apiKey)
-
-  case class Incident(key: String, dateTime: DateTime)
-}
