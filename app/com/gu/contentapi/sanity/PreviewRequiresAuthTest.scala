396d975e650fdeb665c5d24de7047a7978c361c2
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-09-16 15:43:49 +0100

    add longer retry loop

--- app/com/gu/contentapi/sanity/PreviewRequiresAuthTest.scala
+++ app/com/gu/contentapi/sanity/PreviewRequiresAuthTest.scala
@@ -1,18 +1,25 @@
 package com.gu.contentapi.sanity
 
 import org.scalatest.tagobjects.Retryable
-import org.scalatest.time.{Minute, Span}
+import org.scalatest.time.{Minutes, Seconds, Minute, Span}
 import org.scalatest.{Retries, Matchers, FlatSpec}
 import org.scalatest.concurrent.{Eventually, IntegrationPatience, ScalaFutures}
 
 class PreviewRequiresAuthTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience with Retries with Eventually {
 
-  "GETting preview content" should "require authentication" taggedAs(FrequentTest, PRODTest) in {
+  override def withFixture(test: NoArgTest) = {
+    if (isRetryable(test))
+      withRetryOnFailure (Span(30, Seconds))(super.withFixture(test))
+    else
+      super.withFixture(test)
+  }
+
+  "GETting preview content" should "require authentication" taggedAs(FrequentTest, PRODTest, Retryable) in {
 
     handleException {
       //Sometimes this throws a java.io.IOException, with message: Remotely Closed, so using Eventually to retry
-      eventually {
-        val httpRequest = request(Config.previewHost).get
+      eventually(timeout(Span(2, Minutes)),interval(Span(10, Seconds))) {
+        var httpRequest = request(Config.previewHost).get
           whenReady(httpRequest) { result =>
             result.status should be(401)
 
