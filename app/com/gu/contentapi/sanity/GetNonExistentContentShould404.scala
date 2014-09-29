c6e68edaac9b0aa865a21768c3d4f192ee899361
Gideon Goldberg <gidsgoldberg@gmail.com>
2014-09-29 11:48:44 +0100

    don't fail test if CAPI is down

--- app/com/gu/contentapi/sanity/GetNonExistentContentShould404.scala
+++ app/com/gu/contentapi/sanity/GetNonExistentContentShould404.scala
@@ -9,6 +9,7 @@ class GetNonExistentContentShould404 extends FlatSpec with Matchers with ScalaFu
       handleException {
         val httpRequest = requestHost("foo/should-not-exist").get
         whenReady(httpRequest) { result =>
+          assume(result.status != 503)
           result.status should be(404)
         }
       }(fail,testNames.head, tags)
