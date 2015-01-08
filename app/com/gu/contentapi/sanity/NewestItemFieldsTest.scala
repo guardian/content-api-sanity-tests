8680b662c91faa3b671d04ec2e136ff2c547e878
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-01-08 10:47:44 +0000

    add tests for more mandatory fields

--- app/com/gu/contentapi/sanity/NewestItemFieldsTest.scala
+++ app/com/gu/contentapi/sanity/NewestItemFieldsTest.scala
@@ -9,20 +9,33 @@ class NewestItemFieldsTest extends FlatSpec with Matchers with ScalaFutures with
   "The newest item" should "include mandatory fields" taggedAs(FrequentTest, PRODTest)  in {
 
     handleException {
-      val httpRequest = requestHost("search?order-by=newest").get
-      whenReady(httpRequest) { result =>
-        assume(result.status == 200, "Service is down")
-        val json = Json.parse(result.body)
-        val newestItem = (json \ "response" \ "results")(0)
-        val mandatoryFields = List("webTitle","webPublicationDate","sectionName","sectionId","id","webUrl","apiUrl")
-        for(mandatoryField <- mandatoryFields)
-        withClue (s"Mandatory field $mandatoryField was not found \n") {
+      val httpRequest = requestHost("search?order-by=newest&show-tags=all&show-fields=headline").get
+    whenReady(httpRequest) { result =>
+      assume(result.status == 200, "Service is down")
+      val json = Json.parse(result.body)
+      val newestItem = ((json \ "response" \ "results")(0))
+      val newestItemId = (newestItem \ "id").asOpt[String]
+      val mandatoryFields = List[String]("webTitle","webPublicationDate","sectionName","sectionId","id","webUrl","apiUrl")
+      for(mandatoryField <- mandatoryFields)
+        withClue (s"Mandatory field $mandatoryField for $newestItemId was not found \n") {
           (newestItem \ mandatoryField).asOpt[String] should be (defined)
           (newestItem \ mandatoryField).asOpt[String] should not be empty
         }
+      val headline = (newestItem \ "fields" \ "headline").asOpt[String]
+      withClue (s"Mandatory field headline for $newestItemId was not found \n") {
+        headline should be (defined)
+        headline should not be empty
       }
-    }(fail,testNames.head, tags)
-  }
 
+      val newestItemFirstTag = (newestItem \ "tags")(0)
+      val tagMandatoryFields = List("id","webTitle","type","sectionId","sectionName","webUrl","apiUrl")
+      for(tagMandatoryField <- tagMandatoryFields)
+        withClue (s"Mandatory tag field $tagMandatoryField for $newestItemId was not found \n") {
+          (newestItemFirstTag \ tagMandatoryField).asOpt[String] should be (defined)
+          (newestItemFirstTag \ tagMandatoryField).asOpt[String] should not be empty
+        }
 
+    }
+  }(fail,testNames.head, tags)
+ }
 }
