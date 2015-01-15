73b9e84607c3467b94c436fa7880797b96a64081
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-01-15 11:30:16 +0000

    move assumption up so we don't waste an HTTP request.

--- app/com/gu/contentapi/sanity/CrosswordsIndexingTest.scala
+++ app/com/gu/contentapi/sanity/CrosswordsIndexingTest.scala
@@ -13,6 +13,8 @@ class CrosswordsIndexingTest extends FlatSpec with Matchers with ScalaFutures wi
       val now = DateTime.now
       val isAfterChristmas = now.getMonthOfYear == 12 && (now.getDayOfMonth == 26 || now.getDayOfMonth == 27)
       val twentyFiveHoursAgo = now.minusHours(25)
+      assume(!isAfterChristmas, "Cancelling as Crosswords are not always published over the Christmas period")
+
       val httpRequest = requestHost("search?tag=type/crossword&order-by=newest").get
       whenReady(httpRequest) { result =>
         assume(result.status == 200, "Service is down")
@@ -20,7 +22,6 @@ class CrosswordsIndexingTest extends FlatSpec with Matchers with ScalaFutures wi
         val newestItem = (json \ "response" \ "results")(0)
         val newestItemDateString = (newestItem \ "webPublicationDate").as[String]
         val newestItemDate = DateTime.parse(newestItemDateString)
-        assume(!isAfterChristmas, "Cancelling as Crosswords are not always published over the Christmas period")
         withClue(s"Latest indexed crossword was at $newestItemDate which is more than 25 hours ago ($twentyFiveHoursAgo)") {
           newestItemDate.isAfter(twentyFiveHoursAgo) should be(true)
         }
