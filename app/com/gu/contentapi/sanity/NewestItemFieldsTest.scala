1b28e587e876e9884ba44c177c2f4cc80a8325f7
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-01-15 16:39:01 +0000

    check all items in latest for mandatory fields instead of only latest one

--- app/com/gu/contentapi/sanity/NewestItemFieldsTest.scala
+++ app/com/gu/contentapi/sanity/NewestItemFieldsTest.scala
@@ -6,37 +6,49 @@ import play.api.libs.json.{JsValue, Json}
 
 class NewestItemFieldsTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
 
-  "The newest item" should "include mandatory fields" taggedAs(FrequentTest, PRODTest)  in {
+  "The newest items" should "include mandatory fields" taggedAs(FrequentTest, PRODTest) in {
 
     handleException {
-      val httpRequest = requestHost("search?order-by=newest&show-tags=all").get
-    whenReady(httpRequest) { result =>
-      assume(result.status == 200, "Service is down")
-      val json = Json.parse(result.body)
-      val newestItem = ((json \ "response" \ "results")(0))
-      val newestItemFirstTag = (newestItem \ "tags")(0)
-      val newestItemId = (newestItem \ "id").asOpt[String].getOrElse("ID not found")
-      val mandatoryFields = List[JsValue] (
-        newestItem\"webTitle",
-        newestItem\"sectionName",
-        newestItem\"sectionId",
-        newestItem\"id",
-        newestItem\"webUrl",
-        newestItem\"apiUrl",
-        newestItemFirstTag\"id",
-        newestItemFirstTag\"webTitle",
-        newestItemFirstTag\"type",
-        newestItemFirstTag\"sectionId",
-        newestItemFirstTag\"sectionName",
-        newestItemFirstTag\"webUrl",
-        newestItemFirstTag\"apiUrl")
+      val mandatoryItemFields = List[String](
+        "webTitle",
+        "sectionName",
+        "sectionId",
+        "id",
+        "webUrl",
+        "apiUrl")
+      val mandatoryTagFields = List[String](
+        "id",
+        "webTitle",
+        "type",
+        "sectionId",
+        "sectionName",
+        "webUrl",
+        "apiUrl")
 
-      for(mandatoryField <- mandatoryFields)
-        withClue (s"Mandatory field not found! $mandatoryField for ID: $newestItemId") {
-          (mandatoryField).asOpt[String] should be (defined)
-          (mandatoryField).asOpt[String] should not be empty
+      val httpRequest = requestHost("search?order-by=newest&show-tags=all").get
+      whenReady(httpRequest) { result =>
+        assume(result.status == 200, "Service is down")
+        val json = Json.parse(result.body)
+        val newestItemList = (json \ "response" \ "results").as[List[Map[String, JsValue]]]
+        for (item <- newestItemList) {
+          val id = item.getOrElse("id", "ID for item was missing")
+
+          for (mandatoryField <- mandatoryItemFields) {
+            withClue( s"""Mandatory field not found! "$mandatoryField" for item: $id""") {
+              item.contains(mandatoryField) should be(true)
+            }
+          }
+
+          val tags = item.get("tags").get
+          val firstTag = tags(0)
+          val firstTagList = firstTag.as[Map[String, String]]
+          for (mandatoryTagField <- mandatoryTagFields) {
+            withClue( s"""Mandatory tag field not found! "$mandatoryTagField" for item: $id""") {
+              firstTagList.contains(mandatoryTagField)
+            }
+          }
         }
-    }
-  }(fail,testNames.head, tags)
- }
+      }
+    }(fail, testNames.head, tags)
+  }
 }
