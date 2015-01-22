f83a6c7db7bb5489c96c9792eb2bdb04e0abf5ba
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-01-22 12:24:01 +0000

    add is ready within to catch timeout case

--- app/com/gu/contentapi/sanity/AmiSanityTest.scala
+++ app/com/gu/contentapi/sanity/AmiSanityTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.concurrent.{ScalaFutures, IntegrationPatience}
 import play.api.libs.ws.WS
 import play.api.Play.current
+import org.scalatest.time.{Span, Seconds}
 
 
 class AmiSanityTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
@@ -13,6 +14,7 @@ class AmiSanityTest extends FlatSpec with Matchers with ScalaFutures with Integr
 
     handleException {
         val httpRequest = WS.url("https://cloud-images.ubuntu.com/locator/ec2/releasesTable").get
+        assume(httpRequest.isReadyWithin(Span(5, Seconds)),"Service took too long to respond")
         whenReady(httpRequest) { result =>
           assume(result.status == 200, "Service is down")
           withClue (s"EC2 release table did not include $currentAMI, check https://cloud-images.ubuntu.com/locator/ec2/ for latest AMI.") {
