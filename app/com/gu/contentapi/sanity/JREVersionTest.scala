703aef5f70240e0a1a0bc5a0cbda2f251acb06f3
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-01-22 12:39:34 +0000

    catch timeout case and cancel test

--- app/com/gu/contentapi/sanity/JREVersionTest.scala
+++ app/com/gu/contentapi/sanity/JREVersionTest.scala
@@ -1,8 +1,8 @@
 package com.gu.contentapi.sanity
 
-import org.scalatest.{Ignore, Matchers, FlatSpec}
+import org.scalatest.time.{Span, Seconds}
+import org.scalatest.{Matchers, FlatSpec}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
-import org.scalatest.exceptions.TestFailedException
 
 class JREVersionTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
 
@@ -10,8 +10,9 @@ class JREVersionTest extends FlatSpec with Matchers with ScalaFutures with Integ
     "The Content API" should "be using the latest JRE" taggedAs(LowPriorityTest) in {
       handleException {
       val httpRequest = request("http://java.com/applet/JreCurrentVersion2.txt").get
+      assume(httpRequest.isReadyWithin(Span(5, Seconds)))
       whenReady(httpRequest) { result =>
-     assume(result.status == 200, "Service is down")
+      assume(result.status == 200, "Service is down")
       result.body should include("1.8.0_31")
 
 
