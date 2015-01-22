4ce5ef478d845c3dc69bba02768c46ee40c0f3e3
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-01-22 15:57:20 +0000

    move connection into Try

--- app/com/gu/contentapi/sanity/SSLExpiryTest.scala
+++ app/com/gu/contentapi/sanity/SSLExpiryTest.scala
@@ -1,17 +1,20 @@
 package com.gu.contentapi.sanity
 
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import scala.util.{Try, Success, Failure}
 import org.scalatest.{FlatSpec}
 import javax.net.ssl._
-import java.net.{ProtocolException, MalformedURLException, URL}
+import java.net.{URL}
 import org.joda.time.{Days, DateTime}
-import java.io.IOException
 import sun.security.x509.X509CertImpl
 
-class SSLExpiryTest extends FlatSpec {
+import scala.util.Try
 
-  "SSL Certificates" should "be more than 30 days from expiry" taggedAs(LowPriorityTest) in {
+class SSLExpiryTest extends FlatSpec with ScalaFutures with IntegrationPatience {
+
+  "SSL Certificates" should "be more than 30 days from expiry"/* taggedAs (LowPriorityTest)*/ in {
     handleException {
-      val hosts = Seq (
+      val hosts = Seq(
         Config.host,
         Config.hostPublicSecure,
         Config.previewHost,
@@ -19,45 +22,41 @@ class SSLExpiryTest extends FlatSpec {
         Config.writePreviewHost
       )
 
-      val secureHosts = for (host <- hosts) yield host.replaceAll("http://", "https://")
+      val secureHosts = hosts map {
+        _.replaceAll("http://", "https://")
+      }
 
       for (host <- secureHosts) {
-        try {
-          val url = new URL(host)
-          val conn = url.openConnection().asInstanceOf[HttpsURLConnection]
-          conn.setHostnameVerifier(new HostnameVerifier {
-            override def verify(hostnameVerifier: String, sslSession: SSLSession) = true
-          }
-          )
-          conn.connect()
-          val certs = conn.getServerCertificates
-          certs.headOption.map { cert =>
-            val x = cert.asInstanceOf[X509CertImpl]
-            val expiry = x.getNotAfter.getTime
-            val daysleft = Days.daysBetween(new DateTime(), new DateTime(expiry.toLong)).getDays
-            if (daysleft < 30) {
-              fail("Cert for %s expires in %d days".format(host, daysleft))
-            }
-          }
-          conn.disconnect()
-        } catch {
-          case e: MalformedURLException => {
-            fail("Malformed url exception attempting to check ssl cert for %s, exception: %s".format(host, e.getMessage))
-          }
-          case e: ProtocolException => {
-            fail("Protocol exception attempting to check ssl cert for %s, exception: %s".format(host, e.getMessage))
-          }
-          case e: IllegalStateException => {
-            fail("IllegalStateException exception attempting to check ssl cert for %s, exception: %s".format(host, e.getMessage))
-          }
-          case e: NullPointerException => {
-            fail("NullPointerException attempting to check ssl cert for %s, exception: %s".format(host, e.getMessage))
-          }
-          case e: IOException => {
-            fail("IOexception attempting to check ssl cert for %s, exception: %s".format(host, e.getMessage))
-          }
+
+        val url = new URL(host)
+        val conn = url.openConnection().asInstanceOf[HttpsURLConnection]
+        conn.setHostnameVerifier(new HostnameVerifier {
+          override def verify(hostnameVerifier: String, sslSession: SSLSession) = true
         }
+        )
+
+        val certsTry = Try {
+          conn.connect()
+          conn.getServerCertificates
+        }
+        conn.disconnect()
+
+        certsTry match {
+          case Success(certs) =>
+            certs.headOption.map { cert =>
+              val x = cert.asInstanceOf[X509CertImpl]
+              val expiry = x.getNotAfter.getTime
+              val daysleft = Days.daysBetween(new DateTime(), new DateTime(expiry.toLong)).getDays
+              if (daysleft < 30) {
+                fail("Cert for %s expires in %d days".format(host, daysleft))
+              }
+            }
+          case Failure(e) =>
+            cancel(s"Cancelling test as exception thrown: ${e.getClass.getSimpleName}")
+
+        }
+
       }
-    }  (fail,testNames.head, tags)
+    } (fail, testNames.head, tags)
   }
 }
