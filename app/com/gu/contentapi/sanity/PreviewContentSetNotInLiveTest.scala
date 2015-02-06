aa1b6aa82554f431695e39d866bd7bd5989371aa
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-02-06 17:38:18 +0000

    wrap JSON traversal result as option

--- app/com/gu/contentapi/sanity/PreviewContentSetNotInLiveTest.scala
+++ app/com/gu/contentapi/sanity/PreviewContentSetNotInLiveTest.scala
@@ -3,6 +3,7 @@ package com.gu.contentapi.sanity
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{FlatSpec, Matchers}
 import play.api.libs.json.Json
+import org.scalatest.OptionValues._
 
 
 class PreviewContentSetNotInLiveTest extends FlatSpec with Matchers with ScalaFutures with IntegrationPatience {
@@ -14,9 +15,9 @@ class PreviewContentSetNotInLiveTest extends FlatSpec with Matchers with ScalaFu
       whenReady(httpRequest) { result =>
         assume(result.status == 200, "Service is down")
         val json = Json.parse(result.body)
-        val total = (json \ "response" \ "total").as[Long]
+        val total = (json \ "response" \ "total").asOpt[Long]
         val results = Json.stringify(json \ "response" \ "results")
-        total should equal(0)
+        total.value should equal(0)
         results should equal("[]")
       }
     }(fail, testNames.head, tags)
