71cf64ca1a47b7f821e066ea4475ac7ae194e279
Gideon Goldberg <gidsgoldberg@gmail.com>
2015-03-30 12:57:04 +0100

    fix duplicate api-key in query

--- app/com/gu/contentapi/sanity/ContentApiSanityTest.scala
+++ app/com/gu/contentapi/sanity/ContentApiSanityTest.scala
@@ -11,7 +11,7 @@ class ContentApiSanityTest extends FlatSpec with Matchers with ScalaFutures with
     handleException{
       // GZip Compression is enabled in application.conf
 
-      val httpRequest = requestHost("search?format=json&api-key=").get
+      val httpRequest = requestHost("search").get
       whenReady(httpRequest) { result =>
         assume(result.status == 200, "Service is down")
         result.body should include("results")
