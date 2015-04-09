272ce1bc1486465fe9328dac486ebbc6602044ad
Chris Birchall <chris.birchall@guardian.co.uk>
2015-04-09 14:18:18 +0100

    Hmm, test seems to pass sometimes. Use pending instead.

--- test/com/gu/contentapi/sanity/DraftR2ContentShouldAppearInPreviewTest.scala
+++ test/com/gu/contentapi/sanity/DraftR2ContentShouldAppearInPreviewTest.scala
@@ -27,22 +27,22 @@ class DraftR2ContentShouldAppearInPreviewTest extends SanityTestBase(testFailure
   val modifiedHeadline = "Content API Sanity Test " + java.util.UUID.randomUUID.toString
   implicit val webDriver: WebDriver = new HtmlUnitDriver
 
-  // Currently waiting for a response from Stephen Wells about why this is failing
-  pendingUntilFixed {
-    "Updating a draft in R2" should "show an update in the Preview API" taggedAs Retryable in {
-      lazy val tempFilePathString = createModifiedXMLTempFile(Source.fromURL(getClass.getResource("/TestR2IntegrationArticle.xml")).mkString, "Facebook messaging article", modifiedHeadline)
-      login(Config.r2AdminHost + "/tools/newspaperintegration/index")
-      val pageId = postR2ArticleToNewspaperIntegrationEndpoint(tempFilePathString)
-      deleteFileIfExists(tempFilePathString)
+  "Updating a draft in R2" should "show an update in the Preview API" taggedAs Retryable in {
+    // Currently waiting for a response from Stephen Wells about why this is failing
+    pending
 
+    lazy val tempFilePathString = createModifiedXMLTempFile(Source.fromURL(getClass.getResource("/TestR2IntegrationArticle.xml")).mkString, "Facebook messaging article", modifiedHeadline)
+    login(Config.r2AdminHost + "/tools/newspaperintegration/index")
+    val pageId = postR2ArticleToNewspaperIntegrationEndpoint(tempFilePathString)
+    deleteFileIfExists(tempFilePathString)
+
+    eventually(timeout(Span(60, Seconds))) {
+      withClue(s"R2 Article with Page ID:$pageId did not show updated headline $modifiedHeadline within 60 seconds on item endpoint") {
+        isCAPIShowingChange(Config.previewHost + "internal-code/content/" + pageId, modifiedHeadline, Some(Config.previewUsernameCode, Config.previewPasswordCode)) should be(true)
+      }
       eventually(timeout(Span(60, Seconds))) {
-        withClue(s"R2 Article with Page ID:$pageId did not show updated headline $modifiedHeadline within 60 seconds on item endpoint") {
-          isCAPIShowingChange(Config.previewHost + "internal-code/content/" + pageId, modifiedHeadline, Some(Config.previewUsernameCode, Config.previewPasswordCode)) should be(true)
-        }
-        eventually(timeout(Span(60, Seconds))) {
-          withClue(s"R2 Article with Page ID:$pageId did not show updated headline $modifiedHeadline within 60 seconds on search endpoint") {
-            isCAPIShowingChange(Config.previewHost + "search?use-date=last-modified&page-size=100", modifiedHeadline, Some(Config.previewUsernameCode, Config.previewPasswordCode)) should be(true)
-          }
+        withClue(s"R2 Article with Page ID:$pageId did not show updated headline $modifiedHeadline within 60 seconds on search endpoint") {
+          isCAPIShowingChange(Config.previewHost + "search?use-date=last-modified&page-size=100", modifiedHeadline, Some(Config.previewUsernameCode, Config.previewPasswordCode)) should be(true)
         }
       }
     }
