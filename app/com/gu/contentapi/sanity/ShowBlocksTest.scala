cee6e408e56769c309c86c3255d85752b71f68a2
Chris Birchall <chris.birchall@guardian.co.uk>
2015-04-23 10:13:39 +0100

    Only check body blocks list if content has a non-empty body

--- app/com/gu/contentapi/sanity/ShowBlocksTest.scala
+++ app/com/gu/contentapi/sanity/ShowBlocksTest.scala
@@ -1,13 +1,13 @@
 package com.gu.contentapi.sanity
 
 import com.gu.contentapi.sanity.support.TestFailureHandler
-import play.api.libs.json.{JsArray, JsObject, JsValue, Json}
+import play.api.libs.json.{JsArray, JsValue, Json}
 
 class ShowBlocksTest(testFailureHandler: TestFailureHandler) extends SanityTestBase(testFailureHandler) {
 
   "The newest Flex items in the search results" should "include blocks if show-blocks=all" in {
 
-    val httpRequest = requestHost("search?show-blocks=all&show-fields=internalComposerCode").get()
+    val httpRequest = requestHost("search?show-blocks=all&show-fields=internalComposerCode,body").get()
     whenReady(httpRequest) { result =>
       assume(result.status == 200, "Service is down")
       val json = Json.parse(result.body)
@@ -33,7 +33,7 @@ class ShowBlocksTest(testFailureHandler: TestFailureHandler) extends SanityTestB
       newestFlexItem foreach { item =>
         // Second request to the item endpoint for the first flexible content item in the search results
         val itemPath = item("id").as[String]
-        val itemRequest = requestHost(s"$itemPath?show-blocks=all").get()
+        val itemRequest = requestHost(s"$itemPath?show-blocks=all&show-fields=body").get()
         whenReady(itemRequest) { result =>
           assume(result.status == 200, "Service is down")
           val json = Json.parse(result.body)
@@ -46,6 +46,9 @@ class ShowBlocksTest(testFailureHandler: TestFailureHandler) extends SanityTestB
 
   def hasInternalComposerCode(item: Map[String, JsValue]): Boolean =
     item.get("fields").flatMap(fields => (fields \ "internalComposerCode").asOpt[String]).isDefined
+
+  def hasNonEmptyBody(item: Map[String, JsValue]): Boolean =
+    item.get("fields").flatMap(fields => (fields \ "body").asOpt[String]).exists(_.trim.nonEmpty)
 
   def checkBlocks(item: Map[String, JsValue]) = {
     val id = item.getOrElse("id", "ID for item was missing")
@@ -65,13 +68,16 @@ class ShowBlocksTest(testFailureHandler: TestFailureHandler) extends SanityTestB
       bodyBlocks should be (defined)
     }
 
-    // TODO need to filter out cartoons because they have no body blocks
-    //withClue(s"blocks.body is an empty list! for item: $id") {
-      //bodyBlocks.get.value should not be empty
-    //}
-    //withClue(s"first body block has no bodyHtml! for item: $id") {
-      //(bodyBlocks.get.apply(0) \ "bodyHtml").as[String] should not be empty
-    //}
+    // Some content (e.g. cartoons) have an empty body and no body blocks,
+    // so only check body blocks if the body exists and is non-empty.
+    if (hasNonEmptyBody(item)) {
+      withClue(s"blocks.body is an empty list! for item: $id") {
+        bodyBlocks.get.value should not be empty
+      }
+      withClue(s"first body block has no bodyHtml! for item: $id") {
+        (bodyBlocks.get.apply(0) \ "bodyHtml").as[String] should not be empty
+      }
+    }
 
   }
 
