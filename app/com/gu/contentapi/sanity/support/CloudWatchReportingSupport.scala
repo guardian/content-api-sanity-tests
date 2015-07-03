cd8c55dd5b290b0bc947132ed84798061d8a02a4
Chris Birchall <chris.birchall@guardian.co.uk>
2015-07-03 12:50:21 +0100

    Implement the CloudWatch reporter

--- app/com/gu/contentapi/sanity/support/CloudWatchReportingSupport.scala
+++ app/com/gu/contentapi/sanity/support/CloudWatchReportingSupport.scala
@@ -1,6 +1,10 @@
 package com.gu.contentapi.sanity.support
 
-import play.api.Configuration
+import com.amazonaws.handlers.AsyncHandler
+import com.amazonaws.regions.{ServiceAbbreviations, Regions, Region}
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsyncClient
+import com.amazonaws.services.cloudwatch.model.{MetricDatum, PutMetricDataRequest, Dimension}
+import play.api.{Logger, Configuration}
 
 trait CloudWatchReportingSupport {
 
@@ -19,9 +23,67 @@ trait CloudWatchReporter {
 object CloudWatchReporter {
 
   def apply(config: Configuration): CloudWatchReporter = {
-    // TODO
-    DoNothingCloudWatchReporter
+    def cfg(key: String) = config.getString(s"content-api-sanity-tests.cloudwatch-$key")
+    val reporter = for {
+      namespace <- cfg("namespace")
+      stack <- cfg("stack")
+      testRunsMetric <- cfg("test-runs-metric")
+      successfulTestsMetric <- cfg("successful-tests-metric")
+      failedTestsMetric <- cfg("failed-tests-metric")
+    } yield {
+      Logger.info(s"Will report metrics to CloudWatch. namespace=$namespace, stack=$stack, metrics=($testRunsMetric, $successfulTestsMetric, $failedTestsMetric)")
+      new RealCloudWatchReporter(namespace, stack, testRunsMetric, successfulTestsMetric, failedTestsMetric)
+    }
+
+    reporter getOrElse {
+      Logger.info("Will not report any metrics to CloudWatch")
+      DoNothingCloudWatchReporter
+    }
   }
+
+}
+
+class RealCloudWatchReporter(namespace: String,
+                              stack: String,
+                              testRunsMetric: String,
+                              successfulTestsMetric: String,
+                              failedTestsMetric: String) extends CloudWatchReporter {
+
+  private val region = Region.getRegion(Regions.EU_WEST_1)
+
+  lazy val stackDimension = new Dimension().withName("Stack").withValue(stack)
+
+  lazy val cloudwatch = {
+    val client = new AmazonCloudWatchAsyncClient()
+    client.setEndpoint(region.getServiceEndpoint(ServiceAbbreviations.CloudWatch))
+    client
+  }
+
+  object LoggingAsyncHandler extends AsyncHandler[PutMetricDataRequest, Void] {
+    def onError(exception: Exception) {
+      Logger.info(s"CloudWatch PutMetricDataRequest error: ${exception.getMessage}}")
+    }
+    def onSuccess(request: PutMetricDataRequest, result: Void) {
+      Logger.trace("CloudWatch PutMetricDataRequest - success")
+    }
+  }
+
+  private def put(metricName: String, value: Double): Unit = {
+    val metric = new MetricDatum()
+      .withValue(value)
+      .withMetricName(metricName)
+      .withDimensions(stackDimension)
+
+    val request = new PutMetricDataRequest().withNamespace(namespace).withMetricData(metric)
+
+    cloudwatch.putMetricDataAsync(request, LoggingAsyncHandler)
+  }
+
+  override def reportSuccessfulTest(): Unit = put(successfulTestsMetric, 1)
+
+  override def reportFailedTest(): Unit = put(failedTestsMetric, 1)
+
+  override def reportTestRunComplete(): Unit = put(testRunsMetric, 1)
 
 }
 
