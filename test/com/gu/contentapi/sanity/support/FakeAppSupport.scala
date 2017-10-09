0d8d37da812307131fb0a63c2f55cef65826075c
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-10-09 12:02:58 +0100

    Use GuiceOneAppPerSuite

--- test/com/gu/contentapi/sanity/support/FakeAppSupport.scala
+++ test/com/gu/contentapi/sanity/support/FakeAppSupport.scala
@@ -1,9 +1,9 @@
 package com.gu.contentapi.sanity.support
 
 import org.scalatest.{TestData, TestSuite}
-import org.scalatestplus.play.{OneAppPerTest, OneAppPerSuite, FakeApplicationFactory}
+import org.scalatestplus.play.{OneAppPerTest, GuiceOneAppPerSuite, FakeApplicationFactory}
 
-trait FakeAppSupport extends OneAppPerSuite 
+trait FakeAppSupport extends GuiceOneAppPerSuite 
   with FakeApplicationFactory { self: TestSuite =>
 
   override implicit lazy val app = fakeApplication()
