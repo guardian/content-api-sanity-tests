6eb5374823e2c17e4e091eaa1a1ed092d1c90e11
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-10-09 12:23:19 +0100

    Fix broken import

--- test/com/gu/contentapi/sanity/support/FakeAppSupport.scala
+++ test/com/gu/contentapi/sanity/support/FakeAppSupport.scala
@@ -1,7 +1,8 @@
 package com.gu.contentapi.sanity.support
 
 import org.scalatest.{TestData, TestSuite}
-import org.scalatestplus.play.{OneAppPerTest, GuiceOneAppPerSuite, FakeApplicationFactory}
+import org.scalatestplus.play.FakeApplicationFactory
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 
 trait FakeAppSupport extends GuiceOneAppPerSuite 
   with FakeApplicationFactory { self: TestSuite =>
