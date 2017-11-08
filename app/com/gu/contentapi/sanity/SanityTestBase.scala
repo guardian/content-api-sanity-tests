bca394fff89bec255aa57cfaa30dcf599e1479be
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-08 15:32:31 +0000

    Yeah well

--- app/com/gu/contentapi/sanity/SanityTestBase.scala
+++ app/com/gu/contentapi/sanity/SanityTestBase.scala
@@ -1,19 +1,19 @@
 package com.gu.contentapi.sanity
 
 import com.gu.contentapi.sanity.support._
-import org.scalatest.{Matchers, OptionValues, Retries, FlatSpec, BeforeAndAfter}
+import org.scalatest.{Matchers, OptionValues, Retries, FlatSpec, BeforeAndAfterAll}
 import org.scalatest.concurrent.{ScalaFutures, IntegrationPatience, Eventually}
 
 abstract class SanityTestBase(context: Context)
   extends FlatSpec with Matchers with OptionValues
   with Eventually with Retries with ScalaFutures with IntegrationPatience
   with TestFailureHandlingSupport with CloudWatchReportingSupport
-  with HttpRequestSupport with XmlFileSupport with BeforeAndAfter {
+  with HttpRequestSupport with XmlFileSupport with BeforeAndAfterAll {
 
   val testFailureHandler = context.testFailureHandler
   val cloudWatchReporter = context.cloudWatchReporter
 
-  after {
+  override def afterAll = {
     onComplete
   }
 }
