22c8ef92b1ba338c3f224350a1df046044fa5b03
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-11-10 10:02:12 +0000

    Turn object into class

--- app/App.scala
+++ app/App.scala
@@ -2,31 +2,34 @@ import com.gu.contentapi.sanity._
 import com.gu.contentapi.sanity.support.{CloudWatchReporter, InfrequentScheduledTestsFailureHandler, FrequentScheduledTestFailureHandler}
 import com.gu.contentapi.sanity.utils.QuartzScheduler
 import org.joda.time.DateTime
-import scala.concurrent.duration._
+import scala.concurrent.Future, scala.concurrent.duration._
 import scala.language.postfixOps
-import play.api._
+import play.api._, play.api.inject.ApplicationLifecycle
+import play.api.libs.ws.WSClient
 
-object Global extends GlobalSettings {
+class App(
+  appLifecycle: ApplicationLifecycle,
+  cloudWatchReporter: CloudWatchReporter,
+  wsClient: WSClient) {
 
-  override def onStart(app: Application) {
-    val cloudWatchReporter = CloudWatchReporter(app.configuration)
-
+  def start = {
     Logger.info("Application has started")
     QuartzScheduler.start()
     QuartzScheduler schedule("Frequent Tests", runFrequentTests(cloudWatchReporter)) every (30 seconds)
     QuartzScheduler schedule("Infrequent Tests", runInfrequentTests(cloudWatchReporter)) at "0 0 9 ? * MON-FRI *"
-  }
 
-  override def onStop(app: Application) {
-    Logger.info("Application shutdown...")
-    QuartzScheduler.stop()
+    appLifecycle.addStopHook { () =>
+      Logger.info("Application shutdown...")
+      QuartzScheduler.stop()
+      Future.successful(())
+    }
   }
 
   def runFrequentTests(cloudWatchReporter: CloudWatchReporter): Unit = {
     println(s"=== Starting frequent tests at ${new DateTime()} ===")
     val context = Context(testFailureHandler = FrequentScheduledTestFailureHandler, cloudWatchReporter = cloudWatchReporter)
-    val suites = MetaSuites.prodFrequent(context)
-    suites.foreach(_.execute)
+    val suites = MetaSuites.prodFrequent(context, wsClient)
+    suites.foreach(_.execute())
     println(s"=== Frequent tests finished at ${new DateTime()} ===")
     cloudWatchReporter.reportTestRunComplete()
   }
@@ -34,8 +37,8 @@ object Global extends GlobalSettings {
   def runInfrequentTests(cloudWatchReporter: CloudWatchReporter): Unit = {
     println(s"=== Starting infrequent tests at ${new DateTime()} ===")
     val context = Context(testFailureHandler = InfrequentScheduledTestsFailureHandler, cloudWatchReporter = cloudWatchReporter)
-    val suites = MetaSuites.prodInfrequent(context)
-    suites.foreach(_.execute)
+    val suites = MetaSuites.prodInfrequent(context, wsClient)
+    suites.foreach(_.execute())
     println(s"=== Infrequent tests finished at ${new DateTime()} ===")
     cloudWatchReporter.reportTestRunComplete()
   }
